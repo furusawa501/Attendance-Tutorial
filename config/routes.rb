dcb0a32a3ba71cea720d857b150b565368cad359
furusawa501 <furusawa501@gmail.com>
2020-09-08 09:33:32 +0000

    2.7終了　タイトルを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/top'
-
+  # ↑ /static_pages/topというURLに対するGETリクエストを、
+  #  StaticPagesコントローラのtopアクションと結びつけています。
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
