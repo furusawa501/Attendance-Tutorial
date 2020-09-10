cf66f1ab6e20e04c159c072615b3d325caf8757f
furusawa501 <furusawa501@gmail.com>
2020-09-10 03:39:17 +0000

    3 Finish page-layoyt

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # get 'static_pages/top'
   # ↑ /static_pages/topというURLに対するGETリクエストを、
   #  StaticPagesコントローラのtopアクションと結びつけています。
+  get '/signup', to: 'users#new'
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
