d6993316477ed17697ceffa67c72394155d118b4
furusawa501 <furusawa501@gmail.com>
2020-10-07 08:09:54 +0000

    Finish add-user-attendances

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,17 @@
 Rails.application.routes.draw do
-
-  # get 'static_pages/top'
-  # ↑ /static_pages/topというURLに対するGETリクエストを、
-  #  StaticPagesコントローラのtopアクションと結びつけています。
-  
   root 'static_pages#top'
-  # ↑ ルートURL/に対するGETリクエストを、
-  #  StaticPagesコントローラのtopアクションと結びつけています。
   get '/signup', to: 'users#new'
-  
+
   # ログイン機能
-  get     '/login', to:  'sessions#new'
-  post    '/login', to:  'sessions#create'
-  delete  '/logout', to: 'sessions#destroy'
-  
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users do
     member do
       get 'edit_basic_info'
       patch 'update_basic_info'
     end
+    resources :attendances, only: :update # この行を追加します。
   end
 end
